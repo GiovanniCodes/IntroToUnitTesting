37f9d8087cd5f85d8317b2b29d87bf61af12f323
SantiagoMejia183 <32145854+SantiagoMejia183@users.noreply.github.com>
2019-09-09 23:57:42 -0400

    Update test_assginment1.py

--- test_assginment1.py
+++ test_assginment1.py
@@ -19,3 +19,22 @@ class TestSplitAmount(unittest.TestCase):
         self.assertNotEqual(splitAmount(20, 1), [2])
     def test_split_value_error(self):
         self.assertRaises(ValueError,splitAmount, 6 ,'g');
+        
+    def test_bmi_overweight(self):
+        self.assertEqual(bmiCalc(72, 200), "Overweight")
+    def test_bmi_underweight(self):
+        self.assertEqual(bmiCalc(72, 105), "Underweight")
+    def test_bmi_normal(self):
+        self.assertEqual(bmiCalc(72, 170), "Normal")
+    def test_bmi_obese(self):
+        self.assertEqual(bmiCalc(72, 480), "Obese")
+    def test_bmi_value_error(self):
+        self.assertRaises(ValueError, bmiCalc, 'a', 'b', 'c')
+    def test_bmi_negative(self):
+        self.assertEqual(bmiCalc(72, -180), "Negative")
+    def test_distance_zero(self):
+        self.assertEqual(shortestDistance(1,1,1,1), 0)
+    def test_distance_one(self):
+        self.assertAlmostEqual(shortestDistance(1,1,2,2), 1)
+    def test_distance_one(self):
+        self.assertAlmostEqual(shortestDistance(5,10,25,3), 21)
