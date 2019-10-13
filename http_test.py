0a3dfc5459f2114501b3cf85cc59e592f802fc86
SantiagoMejia183 <32145854+SantiagoMejia183@users.noreply.github.com>
2019-10-13 17:25:28 -0400

    Create http_test.py

---
+++ http_test.py
@@ -0,0 +1,19 @@
+import unittest
+from Assignment1 import retirement, splitAmount
+
+def test_request_response(self):
+        response = get_users()
+        self.assertEqual(response.status_code, 508)
+        
+def test_request_response(self):
+        response = get_users()
+        self.assertEqual(response.status_code, 502)
+        
+        
+def test_request_response(self):
+        response = get_users()
+        self.assertEqual(response.status_code, 404)
+        
+def test_request_response(self):
+        response = get_users()
+        self.assertEqual(response.status_code, 200)
